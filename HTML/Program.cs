97973398a3737a8d168d89272530f3cf049fffdb
RavenwindFam <44172162+RavenwindFam@users.noreply.github.com>
2019-02-25 10:23:31 -0500

    Added new code to the search website but its not fully working yet!

---
+++ HTML/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.IO;
+
+namespace test
+{
+    class Handler : BlueberryPie.Handler
+    {
+        [BlueberryPie.Expose(mimetype: "text/plain")]
+        public string postDates (string textdata, Stream avatardata)
+        {
+            return "Your start date is \"" + date1data + "\" and end date is " + date2data;
+        }
+    }
+
+    class MainClass
+    {
+        public static void Main (string[] args)
+        {
+            var srv = new BlueberryPie.Server<Handler>(port:8888, staticFileDir:"files");
+            Console.WriteLine("Listening on 8888");
+            srv.Start();
+        }
+    }
+}
