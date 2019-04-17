e58ecb08ff2f5211e2dcb2f2c8341ca51c630e45
Mallboy <47044742+Mallboy@users.noreply.github.com>
2019-04-17 09:51:44 -0400

    Started the process of the gems being the menu items. Including the foundation for the Upload page being only visible if logged-in.

---
+++ Master/Server_Working/html/menu.js
@@ -0,0 +1,8 @@
+function MenuUpload() {
+  var x = document.getElementById("UploadB");
+  if (x.style.display === "none") {
+    x.style.display = "block";
+  } else {
+    x.style.display = "none";
+  }
+}
