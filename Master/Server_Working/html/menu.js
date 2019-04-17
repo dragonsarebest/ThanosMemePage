84969a17569349414a5d4b3d30db26709979918a
JeffereyAEL <47044769+JeffereyAEL@users.noreply.github.com>
2019-04-17 16:44:27 -0400

    Set up the 'login' button so that if you're signed-in you says 'Account' *** Functionality hasn't been changed

--- Master/Server_Working/html/menu.js
+++ Master/Server_Working/html/menu.js
@@ -1,13 +1,20 @@
+
 function MenuUpload() {
 	var x = document.getElementById("UploadB");
-  
+	var y = document.getElementById("AccountB");
+	var z = document.getElementById("RegisterB");
+	
 	var req = new XMLHttpRequest();
 	req.onload = function() {
 		
 		if (req.response == "-1") {
 			x.style.display = "block";
+			y.style.display = "block";
+			z.style.display = "none";
 		} else {
 			x.style.display = "none";
+			y.style.display = "none";
+			z.style.display = "block";
 		}
 	}
 	
